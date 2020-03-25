4da1342e880a1c4d3673bfcf87ef57a050564e84
Staret <starethub@gmail.com>
2020-03-25 19:55:40 +0700

    test drive info

---
+++ Filazor.Core/Data/FileSystemService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Filazor.Core.Data
+{
+    public class FileSystemService
+    {
+        public Task<DriveInfo[]> GetDriveListAsync()
+        {
+            return Task.Run(() =>
+            {
+                DriveInfo[] driveInfos = DriveInfo.GetDrives();
+
+                return driveInfos;
+            });
+        }
+    }
+}
--- Filazor.Core/Startup.cs
+++ Filazor.Core/Startup.cs
@@ -29,6 +29,7 @@ namespace Filazor.Core
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<FileSystemService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
