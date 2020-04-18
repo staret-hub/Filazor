fac60545ac1599d72d564aa1f856e69d9ff53d75
Staret <starethub@gmail.com>
2020-04-18 17:58:36 +0700

    Test download file

---
+++ Filazor.Core/Pages/Download.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Filazor.Core.Pages
+{
+    public class DownloadModel : PageModel
+    {
+        public IActionResult OnGet(string fileName)
+        {
+            FileInfo fileInfo = new FileInfo(fileName);
+
+            Stream stream = System.IO.File.OpenRead(fileInfo.FullName);
+
+            return File(stream, "application/octet-stream", fileInfo.Name);
+        }
+    }
+}
--- Filazor.Core/Startup.cs
+++ Filazor.Core/Startup.cs
@@ -26,6 +26,7 @@ namespace Filazor.Core
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddRazorPages();
             services.AddServerSideBlazor();
 
