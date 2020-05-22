16b774e8fc7a23ee96b43a52140d94f83565af4c
Staret <starethub@gmail.com>
2020-05-22 18:29:07 +0700

    Updated modal dialog

---
+++ Filazor.Core/Shared/Common.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Filazor.Core.Shared
+{
+    public class Common
+    {
+        public static void DebugPrint(string message)
+        {
+#if DEBUG
+            Console.WriteLine("{0} >>> {1}", DateTime.Now.ToString("HH:mm:ss.fff"), message);
+#endif
+        }
+    }
+}
