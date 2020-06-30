a7e810fd93fd5cb38125e1594b3cb2f4edb96f1f
Staret <starethub@gmail.com>
2020-06-30 16:29:05 +0700

    Test Encrypt Login info

--- Filazor.Core/Data/LoginService.cs
+++ Filazor.Core/Data/LoginService.cs
@@ -3,22 +3,41 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
 
 namespace Filazor.Core.Data
 {
     [AllowAnonymous]
     public class LoginService
     {
-        public bool Login(string id, string password)
+        public static bool Login(string id, string password)
         {
-            if (id == "admin" && password == "Admin1234!")
+            if (string.IsNullOrEmpty(id) || string.IsNullOrEmpty(password))
             {
-                Console.WriteLine("Login admin..");
-
-                return true;
+                Console.WriteLine("Login failed : {0}", "Input was wrong.");
+                
+                return false;
             }
 
-            return false;
+            byte[] salt = Convert.FromBase64String("gq11Pc3RZsEnd2ceMJMisw=="); // MakeSalt(password);
+            //string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(password, salt, KeyDerivationPrf.HMACSHA1, 10000, 256 / 8));
+            string encrypedPassword = Convert.ToBase64String(KeyDerivation.Pbkdf2(password, salt, KeyDerivationPrf.HMACSHA1, 10000, 256 / 8));
+
+            return encrypedPassword == "2+RiUCswdFn6nqsMnSvlhVzdoy11PBj/GKorobJXtDo=";
+        }
+
+        private static byte[] MakeSalt(string password)
+        {
+            byte[] salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+            Console.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
+
+            return salt;
         }
     }
 }
--- Filazor.Core/Pages/Login.cshtml.cs
+++ Filazor.Core/Pages/Login.cshtml.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Filazor.Core.Data;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
 
 namespace Filazor.Core.Pages
 {
@@ -28,9 +30,7 @@ namespace Filazor.Core.Pages
             }
             catch { }
 
-            if (string.IsNullOrEmpty(paramUsername) ||
-                string.IsNullOrEmpty(paramPassword) ||
-                paramUsername != "admin" || paramPassword != "Admin1234!")
+            if (LoginService.Login(paramUsername, paramPassword) == false)
             {
                 Console.WriteLine("Login fail..");
                 return LocalRedirect("/loginControl");
