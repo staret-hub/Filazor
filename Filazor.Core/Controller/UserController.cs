42d85b2dee9caa55c682ea57ae334d16b2ed9c25
Staret <starethub@gmail.com>
2020-07-08 20:09:57 +0700

    Test to get UserInfo

---
+++ Filazor.Core/Controller/UserController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Filazor.Core.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public UserModel GetUser()
+        {
+            // Instantiate a UserModel
+            var userModel = new UserModel
+            {
+                UserName = "",
+                IsAuthenticated = false
+            };
+            // Detect if the user is authenticated
+            if (User.Identity.IsAuthenticated)
+            {
+                // Set the username of the authenticated user
+                userModel.UserName =
+                    User.Identity.Name;
+                userModel.IsAuthenticated =
+                    User.Identity.IsAuthenticated;
+            };
+            return userModel;
+        }
+    }
+
+    public class UserModel
+    {
+        public string UserName { get; set; }
+        public bool IsAuthenticated { get; set; }
+    }
+}
